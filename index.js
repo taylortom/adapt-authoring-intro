72cc3dfdb0fd31ebde080e556ea09667765dc566
Tom Taylor <hello@tomtaylor.name>
2018-06-11 16:11:09 +0100

    Add dashboard steps

--- index.js
+++ index.js
@@ -52,6 +52,50 @@ define(function(require) {
   }
 
   var data = {
-
+    'dashboard': {
+      steps: [
+        {
+          intro: "<h1 class='title'>Welcome to Adapt!</h1><p>Adapt is a free and easy to use e-learning authoring tool that creates fully responsive, multi-device, HTML5 e-learning content using the award-winning Adapt framework.</p><p>Sit back and relax while we give you a whistle-stop tour of the most useful features of the application.</p><p>You can use the buttons below to navigate through the steps (alternatively you can also use the arrow keys). You can leave the tour at any time by pressing Esc.</p><p>Please keep your arms and legs inside the ride at all times.</p>",
+          tooltipClass: 'frontpage',
+        },
+        {
+          intro: "This is the dashboard. Here you'll find the list of Adapt courses that you've already created.",
+          element: '#app'
+        },
+        {
+          intro: "You can customise how the list is displayed using these controls.",
+          element: '.options-inner',
+          position: 'left'
+        },
+        {
+          intro: "These buttons change the list style.",
+          element: '.options-group-layout'
+        },
+        {
+          intro: "These buttons adjust how the list is sorted.",
+          element: '.options-group-sort'
+        },
+        {
+          intro: "You can filter the list using tags.",
+          element: '.projects-sidebar-add-tag',
+          highlightClass: 'dark'
+        },
+        {
+          intro: "Or search for a specific course by name.",
+          element: '.projects-sidebar-filter-search-input',
+          highlightClass: 'dark'
+        },
+        {
+          intro: "By default, you'll only be shown courses you've created, but you can view courses shared by other users by clicking here.",
+          element: '.projects-sidebar-shared-courses-inner',
+          highlightClass: 'dark'
+        },
+        {
+          intro: "Let's get started by adding a new course.",
+          element: '.projects-sidebar-add-course',
+          highlightClass: 'dark'
+        }
+      ]
+    }
   };
 });
